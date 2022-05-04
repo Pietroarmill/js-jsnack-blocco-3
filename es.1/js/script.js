780d0fd172c6f6ba9bb7a2cf1266d374e1a0d968
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-04 13:22:51 +0200

    scaffolding es 1

---
+++ es.1/js/script.js
@@ -0,0 +1,4 @@
+// 1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+const firstNumbersArray = [12, 45, 23, 67, 32, 54, 76];
+const secondNumbersArray = [65, 98, 3, 6];
